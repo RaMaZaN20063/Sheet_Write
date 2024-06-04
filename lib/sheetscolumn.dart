f2b1930bc453ec6cdcb6fd482e6bb012970d69f7
RaMaZaN20063 <abishramazan116@gmail.com>
2024-06-04 14:24:11 +0600

    first commit

---
+++ lib/sheetscolumn.dart
@@ -0,0 +1,8 @@
+class SheetsColumn {
+  static final name = 'name';
+  static final phonenumber = 'phone number';
+  static final school = 'school';
+  static final IIN = 'INN';
+
+  static List<String> getColumns() => [name, phonenumber, school, IIN];
+}
